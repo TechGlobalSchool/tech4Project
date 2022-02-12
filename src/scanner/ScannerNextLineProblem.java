82cbe9eb19a9168c3873b8cf3e648383ee784be0
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-12 15:41:13 -0600

    msg

--- src/scanner/ScannerNextLineProblem.java
+++ src/scanner/ScannerNextLineProblem.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class ScannerNextLineProblem {
     public static void main(String[] args) {
 
-        /*
+        /**
         name        --> next()
         age         --> nextInt()
         myBrainOK   --> nextBoolean()
         fav quote   --> nextLine()
          */
 
-        Scanner userInput = new Scanner(System.in);
+        /*Scanner userInput = new Scanner(System.in);
 
         System.out.println("Whats your name?");
         String name = userInput.next();
@@ -30,6 +30,17 @@ public class ScannerNextLineProblem {
         System.out.println("User name is = " + name +
                 "\nUser age is = " + age +
                 "\nIs user's brain OK? = " + myBrainOK +
-                "\nUser's address is = " + address);
+                "\nUser's address is = " + address);*/
+
+
+        int number = 10; // declaration and assignment
+
+        System.out.println("Number is = " + number); // 10
+
+        number = 20; // re-assignment
+
+        System.out.println("Number is = " + number); // 20
+
+
     }
 }
