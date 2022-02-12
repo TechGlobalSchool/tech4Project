29d49b6f849bf3418597be1e02016f1137d80e7a
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-12 15:35:53 -0600

    me tired

--- src/scanner/ScannerNextLineProblem.java
+++ src/scanner/ScannerNextLineProblem.java
@@ -1,4 +1,35 @@
 package scanner;
 
+import java.util.Scanner;
+
 public class ScannerNextLineProblem {
+    public static void main(String[] args) {
+
+        /*
+        name        --> next()
+        age         --> nextInt()
+        myBrainOK   --> nextBoolean()
+        fav quote   --> nextLine()
+         */
+
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Whats your name?");
+        String name = userInput.next();
+
+        System.out.println("Whats your age?");
+        int age = userInput.nextInt();
+
+        System.out.println("Is your brain fried?");
+        boolean myBrainOK = userInput.nextBoolean();
+
+        System.out.println("Whats your address?");
+        userInput.nextLine();
+        String address = userInput.nextLine();
+
+        System.out.println("User name is = " + name +
+                "\nUser age is = " + age +
+                "\nIs user's brain OK? = " + myBrainOK +
+                "\nUser's address is = " + address);
+    }
 }
