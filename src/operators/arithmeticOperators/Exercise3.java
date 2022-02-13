4570aa7bbac463a7bba0573e58f00cfc5bbe6ad5
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-13 14:18:46 -0600

    msg

--- src/operators/arithmeticOperators/Exercise3.java
+++ src/operators/arithmeticOperators/Exercise3.java
@@ -2,6 +2,27 @@ package operators.arithmeticOperators;
 
 public class Exercise3 {
     public static void main(String[] args) {
+        /*
+        An annual average salary for an SDET in the Unites States is 90k.
+        Write a Java program that calculates and prints the monthly and bi-weekly and
+        weekly average amount that an SDETs makes in the United States.
+         */
 
+        /*
+        Pseudocode:
+        1. Create the variables
+        2. Do the equation
+        3. Print them out
+         */
+
+        double avgSalary = 90_000.00; // 90000.00
+
+        double monthlySal = avgSalary / 12;
+        double biWeeklySal = avgSalary / 26;
+        double weeklySal = avgSalary / 52;
+
+        System.out.println("Monthly salary = " + monthlySal);
+        System.out.println("Bi weekly salary = " + biWeeklySal);
+        System.out.println("Weekly salary = " + weeklySal);
     }
 }
