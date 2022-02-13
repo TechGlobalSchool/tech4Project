ac673590f55c2ae662e1a902c2e159fa995f376e
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-13 14:09:38 -0600

    msg

--- src/operators/arithmeticOperators/Exercise2.java
+++ src/operators/arithmeticOperators/Exercise2.java
@@ -3,5 +3,24 @@ package operators.arithmeticOperators;
 public class Exercise2 {
     public static void main(String[] args) {
 
+        /*
+        Create the variables
+        Find the area with multiplying
+        Find the perimeter with formula (2 * (a + b))
+        Print them out
+         */
+
+        int a = 8;
+        int b = 7;
+
+        int area = a * b;
+//        a = a * b; // a=4, b=6
+        int perimeter = 2 * (a + b);
+
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + perimeter);
+
+
     }
+
 }
---
+++ src/operators/arithmeticOperators/Exercise3.java
@@ -0,0 +1,7 @@
+package operators.arithmeticOperators;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+
+    }
+}
