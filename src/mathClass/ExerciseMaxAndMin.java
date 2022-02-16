2bc5d1338e67affab9ddee4a49b3b05c7e31a4af
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-15 20:51:26 -0600

    msg

---
+++ src/mathClass/ExerciseMaxAndMin.java
@@ -0,0 +1,26 @@
+package mathClass;
+
+public class ExerciseMaxAndMin {
+    public static void main(String[] args) {
+        /*
+        Find the min and max of given 3 numbers, write
+        down the Pseudo code and print min and max with
+        proper messages.
+
+
+        NOTE: Declare and Assign all of them in one single line
+        x = 4, y = 67, z = -54
+         */
+
+//        int x = 4, y = 67, z = -54;
+//
+//        Math.max(x, y); // 67 1st step
+//
+
+        System.out.println("Max of every number is = " +
+                Math.max(Math.max(4, 67), -54)); // 67, -54
+
+        System.out.println("Min of every number is = " +
+                Math.min(Math.min(4, 67), -54));
+    }
+}
--- src/mathClass/FindMin.java
+++ src/mathClass/FindMin.java
@@ -3,5 +3,20 @@ package mathClass;
 public class FindMin {
     public static void main(String[] args) {
 
+        int x = 45;
+        int y = 12;
+
+        int z = 65;
+        int a = 123;
+        int b = -123;
+
+        int minOfXY = Math.min(x ,y); // x, y --> 45, 12
+        int minOfZA = Math.min(z, a); // z, a --> 65, 123
+
+        int minOfZAB = Math.min(minOfZA, b); // za, b --> 65, -123
+        int minOfEvery = Math.min(minOfXY, minOfZAB);
+
+        System.out.println(minOfEvery);
+
     }
 }
