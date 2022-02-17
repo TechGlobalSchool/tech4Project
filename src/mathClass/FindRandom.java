75798a0dbff9fbea28b00b6b4c6c83cee4f15b27
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-16 20:53:41 -0600

    msg

--- src/mathClass/FindRandom.java
+++ src/mathClass/FindRandom.java
@@ -12,5 +12,31 @@ public class FindRandom {
 
         // create a random number between 0 - 20  included take that as an int
 
+        int random0And20Included = (int) (Math.random() * 21);
+
+        System.out.println(random0And20Included);
+
+        // create a random number between 10 - 20  included take that as an int
+        int random10And20Included = (int) (Math.random() * 11) + 10;  // 10 - 20
+
+        System.out.println(random10And20Included);
+
+
+        //create a random number between 103 - 346  included take that as an int
+        // get the difference --> 346 - 103 = 243 add + 1
+        // multiply it by 243 + 1 = 244
+        // add the starting point to the equation
+        // random * (end point - start point + 1) + start point  --> included
+
+        int random103And346Included =103 + (int) (Math.random() * 244);
+
+        System.out.println(random103And346Included);
+
+        // create a random number between -50 / 50  included take that as an int
+        // random * (end point - start point + 1) + start point  --> included
+        // (random * (101)) - 50)
+
+        int random_50And50Included = (int) (Math.random() * 101) - 50;
+        System.out.println(random_50And50Included);
     }
 }
