7e7db198b42cd1318b0bc97d9cc9fb2eb78e2d0c
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-20 15:04:44 -0600

    asd

---
+++ src/ifElseStatements/ifStatements/EvenExample.java
@@ -0,0 +1,23 @@
+package ifElseStatements.ifStatements;
+
+public class EvenExample {
+    /*
+    Write a program that asks user to enter a number.
+    If the number entered is even, then print message “The number you entered is even!”
+
+    0, 2, 4, 6, 8, 10
+
+    EXAMPLE PROGRAM 1
+    Program: Please enter a number?
+    User: 6
+
+    Program: The number you entered is even!
+
+    EXAMPLE PROGRAM 2
+    Program: Please enter a number?
+    User: 13
+
+    Program:
+
+     */
+}
