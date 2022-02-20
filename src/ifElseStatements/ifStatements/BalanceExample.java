1ddd2991323ea833dd3f1243fac6c699b8e050a2
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-20 14:02:16 -0600

    asd

---
+++ src/ifElseStatements/ifStatements/BalanceExample.java
@@ -0,0 +1,23 @@
+package ifElseStatements.ifStatements;
+
+public class BalanceExample {
+    public static void main(String[] args) {
+
+        double balance = 150;
+
+        // you can use relational and logical operator
+        // expression which is going to be true if balance is less than 0
+
+        boolean isBalanceLessThan0 = balance < 0 ;   // false
+        boolean isBalanceEquals100 = balance == 0;      // false
+        boolean isBalanceBiggerOrEqualThan1000 = balance >= 1000;   //false
+
+        // create an expression for checking if balance is not equals to -1
+        boolean isBalanceNotEqual_1 = balance != -1; // true
+
+
+        if(isBalanceNotEqual_1){ // if this is true execute otherwise skip
+            System.out.println("Your balance is not equals to -1");
+        }
+    }
+}
