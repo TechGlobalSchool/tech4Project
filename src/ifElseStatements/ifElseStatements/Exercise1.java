72d860d65da924b6b9cce262b4c0971ef688c2fa
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-22 18:40:28 -0600

    asd

--- src/ifElseStatements/ifElseStatements/Exercise1.java
+++ src/ifElseStatements/ifElseStatements/Exercise1.java
@@ -8,6 +8,8 @@ public class Exercise1 {
         between 45-90. Print out the all the numbers max, min,
         and the middle.
 
+        Logical operators(&&) and relational operators(!=)
+
         56, 78, 63
 
         output:
@@ -20,6 +22,40 @@ public class Exercise1 {
         middle= 63
          */
 
+        int num1 = (int) (Math.random() * 46) + 45;
+        int num2 = (int) (Math.random() * 46) + 45;
+        int num3 = (int) (Math.random() * 46) + 45;
 
+        int max = Math.max(Math.max(num1, num2), num3);
+        int min = Math.min(Math.min(num1, num2), num3);
+        int mid;
+
+
+        if (num1 != max && num1 != min){
+            // num1 is middle, if it is NOT max AND NOT min that means num1 is middle
+            mid = num1;
+        } else if(num2 != max && num2 != min){ // num2 mid possibly is going to be HERE
+            mid = num2;
+        }else{ // num3 mid possibility HERE
+            mid = num3;
+        }
+
+//        if (num2 != max && num2 != min){
+//            // num2 is middle, if it is NOT max AND NOT min that means num1 is middle
+//            mid = num2;
+//        }
+//        if (num3 != max && num3 != min){
+//            // num3 is middle, if it is NOT max AND NOT min that means num1 is middle
+//            mid = num3;
+//        }
+
+        System.out.println(
+                "num1 = " + num1 +
+                "\nnum2 = " + num2 +
+                "\nnum3 = " + num3 +
+                "\nmax = " + max +
+                "\nmin = " + min +
+                "\nmid = " + mid
+        );
     }
 }
