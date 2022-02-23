d2f211fc2402fee47390a37857feecfc0376ec55
Tech Global <techglobal@Techs-iMac.hsd1.il.comcast.net>
2022-02-22 18:46:57 -0600

    asd

---
+++ src/ifElseStatements/ifElseIfStatement/Exercise1.java
@@ -0,0 +1,18 @@
+package ifElseStatements.ifElseIfStatement;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        /*
+        Write a program that generates a random number between -10, 10(inclusive)
+        and if the number is positive print out "number is POSITIVE!",
+        if the number is negative print out "number is NEGATIVE!",
+        and if the number is 0 print out "number is ZERO!".
+
+        RUN1                        RUN2                    RUN3
+        input:                      input:                  input:
+        6                           -5                      0
+        output:                     output:                 output:
+        number is POSITIVE!         number is NEGATIVE!     number is ZERO!
+         */
+    }
+}
