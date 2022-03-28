47e9c8aee5fb0f67e403924fd1da29a6154c7c8d
Tech Global <techglobal@Techs-iMac.local.tld>
2022-03-28 17:06:41 -0500

    added Mock2WhiteBoard class

--- src/exercises/Mock2WhiteBoard.java
+++ src/exercises/Mock2WhiteBoard.java
@@ -6,11 +6,13 @@ public class Mock2WhiteBoard {
     // codingbat arrays1/sum2
 
     public static void main(String[] args) {
-        String pal = "kayak";
+        String pal = "kayaka";
         String sentence = "Java is fun but hard";
         System.out.println("This sentence has " + countWords(sentence) + " words.");
         System.out.println("This sentence has " + countWordsWithSplit(sentence) + " words.");
         System.out.println(isPalindrome(pal));
+        System.out.println(isPalindromeFlag(pal));
+
 
     }
 
@@ -32,6 +34,22 @@ public class Mock2WhiteBoard {
             //s.length() - 1, s.length() - 2,s.length() - 3
         }
         return  s.equals(rS);
+    }
+
+    public static boolean isPalindromeFlag(String s){
+        // civic -> length / 2
+        // toyota -> length / 2 - 1
+        boolean isPalindrome = true;
+        for (int i = 0; i <= s.length() / 2; i++) {
+            //0                 l - 1
+            //1                 l - 2
+            //2                 l - 3
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        return isPalindrome;
     }
 }
 
